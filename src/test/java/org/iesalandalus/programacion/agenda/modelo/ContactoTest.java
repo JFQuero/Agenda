3c3d9b51d63bed978b7f0d20e0d22faf8e1d9ae7
JFQueroDev <JFQueroDev@192.168.1.137>
2018-11-28 20:10:38 +0100

    Ajustadas las expresiones regulares y pasados los tests

--- src/test/java/org/iesalandalus/programacion/agenda/modelo/ContactoTest.java
+++ src/test/java/org/iesalandalus/programacion/agenda/modelo/ContactoTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 public class ContactoTest {
 	
-/*	private static final String MENSAJE_EXCEPCION = "Debería haber saltado la excepción.";
+	private static final String MENSAJE_EXCEPCION = "Debería haber saltado la excepción.";
 
 	@Test
 	public void constructorValidoTest() {
@@ -125,7 +125,7 @@ public class ContactoTest {
 		}
 	}
 	
-	//No pruebo los métodos get y set ya que están cubiertos en las pruebas anteriores
+	/*//No pruebo los métodos get y set ya que están cubiertos en las pruebas anteriores
 	
 	@Test
 	public void toStringTest() {
