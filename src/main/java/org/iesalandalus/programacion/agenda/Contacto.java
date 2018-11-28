070732495530ad7a268acdf2919e356658d42094
JFQueroDev <JFQueroDev@192.168.1.137>
2018-11-28 19:02:33 +0100

    Creados los metodos get y set para los atributos y sus excepciones

--- src/main/java/org/iesalandalus/programacion/agenda/Contacto.java
+++ src/main/java/org/iesalandalus/programacion/agenda/Contacto.java
@@ -1,7 +1,58 @@
 package org.iesalandalus.programacion.agenda;
 
 public class Contacto {
+
+	private static final String ER_TELEFONO = ("([9]{1}[0-9]{8})");
+	private static final String ER_CORREO = ("(\\w+[.*\\w]+@+\\w+.\\w{2,5})");
+
 	private String nombre;
 	private String telefono;
 	private String correo;
+
+	/* Metodos para nombre */
+	public String getNombre() {
+		return nombre;
+	}
+
+	private void setNombre(String nombre) {
+		if (this.nombre != null) {
+			throw new IllegalArgumentException("No se puede cambiar el nombre de un Contacto existente");
+		} else {
+			if (nombre == null | nombre == "") {
+				throw new IllegalArgumentException("El nombre de un contacto no puede ser nulo o vacío.");
+			} else {
+				this.nombre = nombre;
+			}
+		}
+	}
+
+	/* Metodos para telefono */
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		if (telefono == null | telefono == "") {
+			throw new IllegalArgumentException("El teléfono de un contacto no puede ser nulo o vacío.");
+		} else {
+			if (telefono.matches(ER_TELEFONO)) {
+				this.telefono = telefono;
+			}
+		}
+	}
+
+	/* Metodos para correo */
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		if (correo == null | correo == "") {
+			throw new IllegalArgumentException("El correo de un contacto no puede ser nulo o vacío.");
+		} else {
+			if (correo.matches(ER_CORREO)) {
+				this.correo = correo;
+			}
+		}
+	}
 }
