8ee433fc9078c9917124455c427ba517f3de39a4
JFQueroDev <JFQueroDev@192.168.1.136>
2018-12-04 19:25:24 +0100

    Creada la clase Agenda y sus atributos

---
+++ src/test/java/org/iesalandalus/programacion/agenda/modelo/Agenda.java
@@ -0,0 +1,9 @@
+package org.iesalandalus.programacion.agenda.modelo;
+
+public class Agenda {
+
+	private static final int MAX_CONTACTOS = 99;
+	private int numContactos;
+	private Contacto[] Contactos; //Array creado por la flecha del diagrama de clases
+	
+}
