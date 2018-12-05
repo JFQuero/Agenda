c2b559fcf255c10e22b2701d9b5f74345b92f6b2
JFQueroDev <JFQueroDev@192.168.1.136>
2018-12-05 16:52:09 +0100

    Creados los metodos get para Agenda

--- src/test/java/org/iesalandalus/programacion/agenda/modelo/Agenda.java
+++ src/test/java/org/iesalandalus/programacion/agenda/modelo/Agenda.java
@@ -1,9 +1,31 @@
 package org.iesalandalus.programacion.agenda.modelo;
 
+import java.util.Arrays;
+
 public class Agenda {
 
 	private static final int MAX_CONTACTOS = 99;
 	private int numContactos;
-	private Contacto[] Contactos; //Array creado por la flecha del diagrama de clases
-	
+	private Contacto[] contactos; // Array creado por la flecha del diagrama de clases
+
+	/* Constructores */
+	public Agenda() {
+		contactos = new Contacto[MAX_CONTACTOS];
+	}
+
+	/* Metodos get */
+	public Contacto[] getContactos() {
+		Contacto[] copiaContactos = Arrays.copyOf(contactos, contactos.length);
+		return copiaContactos;
+	}
+
+	public int getNumContactos() {
+		for (int i = 0; 0 < contactos.length; i++) {
+			if (contactos[i] != null) {
+				numContactos++;
+			}
+		}
+		return numContactos;
+	}
+
 }
